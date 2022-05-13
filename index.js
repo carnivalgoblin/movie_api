6a01bb0f76c94cea5d73cbda55b2c979944cee2c
carnivalgoblin <rico.prodan@gmail.com>
2022-05-13 12:42:33 +0200

    added REST API endpoints

--- index.js
+++ index.js
@@ -1,64 +1,101 @@
 const express = require('express'),
     morgan = require('morgan');
-/* fs = require('fs'),
-path = require('path'); */
 
 const app = express();
 
 app.use(morgan('common'));
-/* const accesLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), { flags: 'a' })
-
-app.use(morgan('combined', { stream: accesLogStream })); */
 
 let topMovies = [
     {
         title: 'The Great Escape',
-        year: '1963'
+        year: '1963',
+        genre: 'Thriller'
     },
     {
         title: 'The Magnificent Seven',
-        year: '1960'
+        year: '1960',
+        genre: 'Western'
     },
     {
         title: 'Back to the Future',
-        year: '1985'
+        year: '1985',
+        genre: 'Sci-Fi'
     },
     {
         title: 'Back to the Future II',
-        year: '1989'
+        year: '1989',
+        genre: 'Sci-Fi'
     },
     {
         title: 'Back to the Future III',
-        year: '1990'
+        year: '1990',
+        genre: 'Sci-Fi'
     },
     {
         title: 'Indiana Jones: Raiders of the Lost Ark)',
-        year: '1981'
+        year: '1981',
+        genre: 'Thriller'
     },
     {
         title: "Marvel's The Avengers",
-        year: '2012'
+        year: '2012',
+        genre: 'Action'
     },
     {
         title: 'Forrest Gump',
-        year: '1994'
+        year: '1994',
+        genre: 'Drama'
     },
     {
         title: 'Home Alone',
-        year: '1990'
+        year: '1990',
+        genre: 'Family'
     },
     {
         title: 'E.T.',
-        year: '1982'
+        year: '1982',
+        genre: 'Sci-Fi'
     }
 ]
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the Movie Club!');
+});
 
 app.get('/movies', (req, res) => {
     res.json(topMovies);
 });
 
-app.get('/', (req, res) => {
-    res.send('Welcome to the Movie Club!');
+app.get('/movies/:title', (req, res) => {
+    res.send('Successful GET request returning data on a single movie.');
+});
+
+app.get('/movies/genre/:genre', (req, res) => {
+    res.send('Successful GET request returning data on a genre.');
+});
+
+app.get('/directors/:name', (req, res) => {
+    res.send('Successful GET request returning data on a director by name.')
+});
+
+app.post('/users/register', (req, res) => {
+    res.send('Successful POST request for adding user.')
+});
+
+app.put('/users/update/:id', (req, res) => {
+    res.send('Successful PUT request for updating user info.')
+});
+
+app.post('/users/favourites/add/:id/:title', (req, res) => {
+    res.send('Successful POST request for adding a movie to the favorites list.')
+});
+
+app.delete('/users/favourites/remove/:id/:title', (req, res) => {
+    res.send('Successful DELETE request for removing a movie from the favorites list.')
+});
+
+app.delete('/users/deregister/:id', (req, res) => {
+    res.send('Successful DELETE request for removing user.')
 });
 
 app.use(express.static('public'));
