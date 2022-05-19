36d651262ca50e71c4b2ca10e63898b1e43a1741
carnivalgoblin <rico.prodan@gmail.com>
2022-05-19 14:11:04 +0200

    add connection to MongoDB Atlas cluster

--- index.js
+++ index.js
@@ -31,7 +31,8 @@ require('./passport');
 const Movies = Models.Movie;
 const Users = Models.User;
 
-mongoose.connect('mongodb://localhost:27017/flixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+/* mongoose.connect('mongodb://localhost:27017/flixDB', { useNewUrlParser: true, useUnifiedTopology: true }); */
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.get('/', (req, res) => {
     res.send('Welcome to the Movie Club!');
